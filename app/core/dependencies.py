6f7256fb56fa306d157eb3cdf9b989928f630381
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-02-14 00:38:48 +0000

    feat: Add OAuth2PasswordBearer dependency for login and authentication

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -5,8 +5,13 @@
 from typing import Annotated
 
 from fastapi import Depends
-from sqlmodel import Session
+from fastapi.security import OAuth2PasswordBearer
 
+from app.core.config import settings
 from app.core.database import Session, get_session
 
 DBSessionDependency = Annotated[Session, Depends(get_session)]
+
+OAuth2SchemeDependency = Annotated[
+    str, Depends(OAuth2PasswordBearer(tokenUrl=settings.login_route))
+]
